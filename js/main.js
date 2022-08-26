de22f4dffd52f85c49b7b78f53996cf3c26ef9a1
arman.p <arman.p@F4RPDEV-62>
2022-08-26 10:58:29 +0400

    Added exrecises

---
+++ js/main.js
@@ -0,0 +1,87 @@
+'use strict';
+
+
+// let array = [5, 10, 33];
+//
+// let secondArray = [];
+//
+// for (let i = 0; i < array.length; i++) {
+//     array[i] *= 2;
+//     secondArray[i] = array[i];
+// }
+// alert(secondArray);
+
+// let array = [];
+//
+// array[0] = 9;
+// array[1] = "text";
+// array[10] = "ok";
+//
+// alert(array[9]);
+//
+// alert(array.length);
+
+// let array = [5, 8, 19, 340, 24, 58, 919, 82, 11, 33];
+//
+// let onlyOddNumber = [];
+//
+// for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 !== 0) {
+//         onlyOddNumber.push(array[i]) ;
+//     }
+// }
+
+// for (let i = 0; i < onlyOddNumber; i++) {
+//     if (onlyOddNumber[i] === undefined) {
+//         onlyOddNumber.shift();
+//     }
+// }
+
+// alert(onlyOddNumber);
+
+// let randArray = [1, 22, 32, 20, 57, 44, 11, 321, 120, 111, 91, 37];
+//
+// let onlyEvenNumbers = [];
+//
+// for (let i = 0; i < randArray.length; i++) {
+//     if (randArray[i] % 2 === 0) {
+//         onlyEvenNumbers.push(randArray[i]);
+//     }
+// }
+// alert(onlyEvenNumbers);
+
+// let sum = function (x, z) {
+//     return x + z;
+// };
+//
+// let result = sum(10, 20);
+// alert(result);
+
+
+// function FindEvenNumber(array, readyArray) {
+//     for (let i = 0; i < array.length; i++) {
+//         if (array[i] % 2 === 0) {
+//             readyArray.push(array[i]);
+//         }
+//     }
+//     alert(readyArray);
+// }
+//
+// let array = [1, 22, 32, 20, 11, 64, 33, 37, 79, 81, 121, 311];
+//
+// let newArray = [];
+//
+// FindEvenNumber(array, newArray);
+
+// function sumAllElements(array) {
+//     let element = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         element += array[i];
+//     }
+//     alert(element);
+// }
+//
+// let array = [1, 10, 30, 32, 9, 12, 101, 99, 36, 24];
+
+
+// sumAllElements(array);
